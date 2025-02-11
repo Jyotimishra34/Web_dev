4623de6b1314f172fbe926e4cc1e37081a1df097
Jyoti Mishra <mishrajyoti9131@gmail.com>
2025-02-11 07:34:28 +0530

    js

---
+++ CodeTrail/JavaScript/first.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Meri Kaksha");
+fullName= " Sweta Mishra";
+_fullName= " Vivek Thakur";
+$fullName= " Mahi ";
+age= 40;
+price = 45;
+x= null;
+y= undefined;
+isFollow = false;
+console.log(y);
+console.log(isFollow);
+console.log(fullName);
+console.log(_fullName);
+console.log($fullName);
